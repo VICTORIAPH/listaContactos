08383bbc57c30393754a1a4054260f4d4196fc54
victoria <vicky454ph@mail.com>
2022-05-04 09:52:41 -0500

    contacto

--- ContactosPM/EditarViewController.swift
+++ ContactosPM/EditarViewController.swift
@@ -8,13 +8,35 @@
 import UIKit
 
 class EditarViewController: UIViewController {
-
-    var recibirNombre: String?
+   
+    @IBOutlet weak var recibirImage: UIImageView!
+    
+    @IBOutlet weak var recibirTele: UITextField!
+    
+    @IBOutlet weak var recibirDireccion: UITextField!
+    @IBOutlet weak var recibirEmail: UITextField!
+    
+    @IBOutlet weak var btnCancelar: UIButton!
+    
+    @IBOutlet weak var btnAceptar: UIButton!
+    //var recibirNombre: String?
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
    
+    @IBAction func btnCancelar(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnAceptar(_ sender: UIButton) {
+    }
+    
+    extension EditarViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+        func imagePickerController.In(<#parameters#>) -> <#return type#> {
+            <#function body#>
+        }
+    }
 
+    
 }
